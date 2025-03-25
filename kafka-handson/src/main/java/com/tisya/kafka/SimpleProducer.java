b51745be7a09d1c2efeb7447c983a68f30195090
tisyasolutions <raveendra.murthy@tisyasolutions.com>
2025-03-25 10:40:04 +0530

    Added Support for Kafka 4.0.0
    Added Support for Kafka 4.0.0

---
+++ kafka-handson/src/main/java/com/tisya/kafka/SimpleProducer.java
@@ -0,0 +1,24 @@
+package com.tisya.kafka;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+public class SimpleProducer {
+    public static void main(String[] args) {
+        String topicName = "hello-topic";
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "kafka-1:19092,kafka-2:29092,kafka-3:39092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("acks", "all");
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+        for (int i = 0; i < 10; i++) {
+            String key = "Key" + i;
+            String message = "Message from KafkaApp" + i;
+            producer.send(new ProducerRecord<String, String>(topicName, key, message));
+        }
+        producer.close();
+        System.out.println("Message sent successfully");
+    }
+}
