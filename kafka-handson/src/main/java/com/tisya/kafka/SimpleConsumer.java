5f0048596711edc5afe34413e83acdc773ab164e
tisyasolutions <raveendra.murthy@tisyasolutions.com>
2025-04-09 11:43:17 +0530

    Added version in yml

--- kafka-handson/src/main/java/com/tisya/kafka/SimpleConsumer.java
+++ kafka-handson/src/main/java/com/tisya/kafka/SimpleConsumer.java
@@ -1,10 +1,12 @@
 package com.tisya.kafka;
 
-import java.util.*;
-import java.io.*;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 public class SimpleConsumer{
 
@@ -26,7 +28,7 @@ public class SimpleConsumer{
             consumer.subscribe(Arrays.asList(topicName));
 
             while (true){
-                ConsumerRecords<String, String> records = consumer.poll(100);
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
                 for (ConsumerRecord<String, String> record : records){
                 	System.out.printf("Message received -> partition = %d, offset = %d, key = %s, value = %s\n", record.partition(), record.offset(), record.key(), record.value());
                 }
